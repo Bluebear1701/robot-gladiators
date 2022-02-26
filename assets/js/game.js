38f75b85ec6b1172d1b63648af8c3e0e7ce7ff61
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-02-26 02:28:03 -0600

    added player stats and startGame

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,6 +12,7 @@ console.log(enemyNames.length);
 console.log(enemyNames[0]);
 console.log(enemyNames[3]);
 
+
 // fight function (now with parameter for enemy's name)
 var fight = function(enemyName) {
   while (playerHealth > 0 && enemyHealth > 0) {
@@ -69,40 +70,27 @@ var fight = function(enemyName) {
   }
 };
 
-if (playerHealth > 0) {
-  //let player know what round they are in, remember that arrays start at 0 so it needs to have a 1 added to it
-  window.alert("Welcome to Robot Gladiators!" +(i + 1) );
-  
-  //pick new enemy to fight based on the index of the enemyNames array
-  var pickedEnemyName = enemyNames[i];
-  
-  //reset enemyHealth before starting new fight
-  enemyHealth = 50;
-
-  //use debugger to pause script from running and check what's going on at that moment in the code
-  //debugger
-
-  // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
-  fight(pickedEnemyName);
-}
 // fight each enemy-robot by looping over them and fighting them one at a time
+var startGame = function() {  
+  playerHealth = 100
+  playerAttack = 10
+  playerMoney = 10
+  
 for (var i = 0; i < enemyNames.length; i++) {
   // if player is still alive, keep fighting
   if (playerHealth > 0) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
-    window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
 
     // pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
 
     // reset enemyHealth before starting new fight
-    enemyHealth = 50;
-
-    // use debugger to pause script from running and check what's going on at that moment in the code
-    // debugger;
+    enemyHealth = 50;    
 
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
+    
   }
   // if player isn't alive, stop the game
   else {
@@ -110,3 +98,7 @@ for (var i = 0; i < enemyNames.length; i++) {
     break;
   }
 }
+}
+
+//play again
+startGame();
